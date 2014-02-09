86f4e6e10d1894d2098b3f5d40e8654f1fda60dc
Tushar Makkar <tusharmakkar08@gmail.com>
2014-02-09 13:00:05 +0530

    Comments added on request

--- download.py
+++ download.py
@@ -25,22 +25,32 @@ import urllib,os,time
 
 def main():
 	url = "http://haryana.gov.in/pratibha-samman/images/DSC_0"
+	# Basic source of downloading
 	for i in xrange(391,570):
+		# Numbers in range is found by the hardcore method of trial and 
+		# error
 		suffix=str(i)+".jpg"
 		url+=suffix
 		uopen = urllib.urlopen(url)
+		# For Opening Url
 		stream = uopen.read()
 		di=os.getcwd()+"/../haryana/"
 		if not os.path.exists(di):
 			os.makedirs(di)
+		# Making of new directory if doesn't exists
 		if os.path.exists(di+str(i-390)+".jpg"):
 			continue
+		# Download if and only if the photo not downloaded earlier
 		file = open(di+str(i-390)+".jpg",'w')
 		file.write(stream)
+		# Writing stream to local system
 		file.close()
 		time.sleep(1)
+		# Sleep because python is too fast to not be detected as an attack
+		# on government site :P 
 		url=url[:-7]
 		print "Downloaded Image Number: "+str(i-390)
+	time.sleep(1)
 	return 0
 
 if __name__ == '__main__':
